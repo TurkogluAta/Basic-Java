00de77cb8622d3617ca2f4bf26aeac683c2ca992
Ata Turkoglu <116079184+TurkogluAta@users.noreply.github.com>
2023-10-16 14:10:20 +0100

    Add files via upload

---
+++ MyInitials.java
@@ -0,0 +1,18 @@
+/*
+* MyInitials.java
+* @author: Ata Turkoglu
+* Date: 24/01/16 
+*/
+
+public class MyInitials {
+    public static void main(String args[]){
+        System.out.println();
+        System.out.println("      A      TTTTTTTTT");
+        System.out.println("     A A         T");
+        System.out.println("    A   A        T");
+        System.out.println("   AAAAAAA       T");
+        System.out.println("  A       A      T");
+        System.out.println(" A         A     T");
+        System.out.println();
+    }
+}
---
+++ TimesTables.java
@@ -0,0 +1,23 @@
+/*
+* TimesTables.java
+* @author: Ata Turkoglu
+* Date: 24/01/16 
+*/
+
+public class TimesTables {
+    public static void main(String args[]){
+        System.out.println("3 times 0 is " + (3*0));
+        System.out.println("3 times 1 is " + (3*1));
+        System.out.println("3 times 2 is " + (3*2));
+        System.out.println("3 times 3 is " + (3*3));
+        System.out.println("3 times 4 is " + (3*4));
+        System.out.println("3 times 5 is " + (3*5));
+        System.out.println("3 times 6 is " + (3*6));
+        System.out.println("3 times 7 is " + (3*7));
+        System.out.println("3 times 8 is " + (3*8));
+        System.out.println("3 times 9 is " + (3*9));
+        System.out.println("3 times 10 is " + (3*10));
+        System.out.println("3 times 11 is " + (3*11));
+        System.out.println("3 times 12 is " + (3*12));
+    }
+}
